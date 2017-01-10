d8f47a3a1dd3437515f098c174637390ad4b8fd1
Raghu Betina <raghu@firstdraft.com>
2017-01-10 19:16:21 +0000

    Direct association between reviews and userphotos

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,10 @@
 class Review < ApplicationRecord
   # Direct associations
 
+  has_many   :reviewphotos,
+             :class_name => "Userphoto",
+             :dependent => :destroy
+
   belongs_to :user
 
   belongs_to :restaurant
